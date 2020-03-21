163000e3bd60711807e9377ba0c69f69bd59eaba
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-03-21 22:21:53 +0200

    Updates

---
+++ precache-manifest.cd24947d386275e9ed2e9119f98d2e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf358039ee1c817eab89cd6b5053e88",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "f0cf170e201c49218ead",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.64d182d2.chunk.css"
+  },
+  {
+    "revision": "f37fab53ef0c13c73b67",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.d2cb301d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.d2cb301d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cf170e201c49218ead",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.2c164319.chunk.js"
+  },
+  {
+    "revision": "4710f3dcc596b7874034",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.3adcab40.js"
+  }
+]);
